d930caddf9334d0eaf7e63ac47012b7bc6c64c5a
ryan <ryan-1@163.com>
2019-12-02 23:37:31 +0800

    Add spring boot module.

---
+++ dubbo-movie-provider-boot/src/main/java/com/ryan/dubbo/DubboMovieProviderBootApplication.java
@@ -0,0 +1,13 @@
+package com.ryan.dubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboMovieProviderBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboMovieProviderBootApplication.class, args);
+    }
+
+}
---
+++ dubbo-user-consumer-boot/src/main/java/com/ryan/dubbo/DubboUserConsumerBootApplication.java
@@ -0,0 +1,13 @@
+package com.ryan.dubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboUserConsumerBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboUserConsumerBootApplication.class, args);
+    }
+
+}
